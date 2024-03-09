2408507e11f38e689ab5712f2f6bec98ba3ef06c
Gvozdem <gvozdenko.demid@mail.ru>
2024-03-09 02:22:58 +0200

    add task 2

--- Lab_2/index.php
+++ Lab_2/index.php
@@ -10,3 +10,22 @@ $order .= " and a lot of things";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+
+//task_2
+$intNumber = 5;
+echo "\n$intNumber";
+
+echo "\n";
+
+$floatNumber = 2.4;
+echo $floatNumber;
+
+echo "\n";
+echo $intNumber * $floatNumber;
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "\n";
+echo $lastMonth - $thisMonth;
+
